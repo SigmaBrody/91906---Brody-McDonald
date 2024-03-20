1ce17445f2d215c257175bc3b4e89710855d0a4a
Brody McDonald <bm20251@my.westlake.school.nz>
2024-03-20 14:39:57 +1300

    Character edits and animations

--- alphaprogramz.py
+++ alphaprogramz.py
@@ -32,6 +32,101 @@ LAYER_NAME_BACKGROUND = "Background"
 LAYER_NAME_DONT_TOUCH = "Dont Touch"
 LAYER_NAME_LADDERS = "Ladders"
 LAYER_NAME_MOVING_PLATFORMS = "Moving Platforms"
+
+class PlayerCharacter(arcade.Sprite):
+    """Player Sprite"""
+
+    def __init__(self):
+
+        # Set up parent class
+        super().__init__()
+
+        # Default to face-right
+        self.character_face_direction = RIGHT_FACING
+
+        # Used for flipping between image sequences
+        self.cur_texture = 0
+        self.scale = CHARACTER_SCALING
+
+        # Track our state
+        self.jumping = False
+        self.climbing = False
+        self.is_on_ladder = False
+
+        # --- Load Textures ---
+
+        # Images from Kenney.nl's Asset Pack 3
+        main_path = ":resources:images/animated_characters/male_person/malePerson"
+
+        # Load textures for idle standing
+        self.idle_texture_pair = load_texture_pair(f"{main_path}_idle.png")
+        self.jump_texture_pair = load_texture_pair(f"{main_path}_jump.png")
+        self.fall_texture_pair = load_texture_pair(f"{main_path}_fall.png")
+
+        # Load textures for walking
+        self.walk_textures = []
+        for i in range(8):
+            texture = load_texture_pair(f"{main_path}_walk{i}.png")
+            self.walk_textures.append(texture)
+
+        # Load textures for climbing
+        self.climbing_textures = []
+        texture = arcade.load_texture(f"{main_path}_climb0.png")
+        self.climbing_textures.append(texture)
+        texture = arcade.load_texture(f"{main_path}_climb1.png")
+        self.climbing_textures.append(texture)
+
+        # Set the initial texture
+        self.texture = self.idle_texture_pair[0]
+
+        # Hit box will be set based on the first image used. If you want to specify
+        # a different hit box, you can do it like the code below.
+        # set_hit_box = [[-22, -64], [22, -64], [22, 28], [-22, 28]]
+        self.hit_box = self.texture.hit_box_points
+
+    def update_animation(self, delta_time: float = 1 / 60):
+
+        # Figure out if we need to flip face left or right
+        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
+            self.character_face_direction = LEFT_FACING
+        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
+            self.character_face_direction = RIGHT_FACING
+
+        # Climbing animation
+        if self.is_on_ladder:
+            self.climbing = True
+        if not self.is_on_ladder and self.climbing:
+            self.climbing = False
+        if self.climbing and abs(self.change_y) > 1:
+            self.cur_texture += 1
+            if self.cur_texture > 7:
+                self.cur_texture = 0
+        if self.climbing:
+            self.texture = self.climbing_textures[self.cur_texture // 4]
+            return
+
+        # Jumping animation
+        if self.change_y > 0 and not self.is_on_ladder:
+            self.texture = self.jump_texture_pair[self.character_face_direction]
+            return
+        elif self.change_y < 0 and not self.is_on_ladder:
+            self.texture = self.fall_texture_pair[self.character_face_direction]
+            return
+
+        # Idle animation
+        if self.change_x == 0:
+            self.texture = self.idle_texture_pair[self.character_face_direction]
+            return
+
+        # Walking animation
+        self.cur_texture += 1
+        if self.cur_texture > 7:
+            self.cur_texture = 0
+        self.texture = self.walk_textures[self.cur_texture][
+            self.character_face_direction
+        ]
+
+
 class MyGame(arcade.Window):
     """
     Main application class.
@@ -84,8 +179,8 @@ class MyGame(arcade.Window):
         os.chdir(file_path)
         # Name of map file to load
 
-        map_name = f"map_{self.level}.tmx"
-
+        # map_name = f"map_{self.level}.tmx"
+        map_name = "map_3.tmx"
 
 
         # Layer specific options are defined based on Layer names in a dictionary
@@ -125,7 +220,6 @@ class MyGame(arcade.Window):
         self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
 
         self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
-        print(self.end_of_map)
 
         # Initialize Scene with our TileMap, this will automatically add all layers
 
@@ -185,7 +279,7 @@ class MyGame(arcade.Window):
 
         # Draw our Scene
 
-        self.scene.draw()
+        self.scene.draw(pixelated=True)
 
 
         # Activate the GUI camera before drawing GUI elements
@@ -255,7 +349,7 @@ class MyGame(arcade.Window):
         # See if the user got to the end of the level
         if self.player_sprite.center_x >= self.end_of_map:
             # Advance to the next level
-            self.level += 1
+            self.level = self.level + 1 
             self.setup()
 
 
